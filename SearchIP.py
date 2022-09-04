54884de6ce121a27313d605818badb4ecb7b1a33
Oswaoldo Hernandez Avila <108857178+420Avila@users.noreply.github.com>
2022-09-04 14:35:34 -0500

    Update SearchIP.py

--- SearchIP.py
+++ SearchIP.py
@@ -1,7 +1,11 @@
 import socket
+import getpass  
 
+user = getpass.getuser()
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
 
+
 print("El nombre de tu ordenador es: " + hostname)
 print("tu direción IP es: " + ip)
+print("tu nombre de usuario es: " + user)
